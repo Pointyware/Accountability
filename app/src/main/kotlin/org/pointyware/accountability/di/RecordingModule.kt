2d0043c04ee7277d6fddceddc7edd5fe0eb6249f
Taush Sampley <taushsampley@gmail.com>
2024-09-24 16:15:19 -0500

    Add hilt recording module

---
+++ app/src/main/kotlin/org/pointyware/accountability/di/RecordingModule.kt
@@ -0,0 +1,61 @@
+package org.pointyware.accountability.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.pointyware.accountability.audio.AudioDao
+import org.pointyware.accountability.audio.AudioOptionsDataSource
+import org.pointyware.accountability.recording.AndroidRecordingRepository
+import org.pointyware.accountability.recording.LocalRecordingDao
+import org.pointyware.accountability.recording.RecordingManager
+import org.pointyware.accountability.recording.RecordingRepository
+import org.pointyware.accountability.recording.camera.CameraRecordingManager
+import org.pointyware.accountability.settings.AndroidConfigurationRepository
+import org.pointyware.accountability.settings.CallingOptionsAndroidDataSource
+import org.pointyware.accountability.settings.CallingOptionsDataSource
+import org.pointyware.accountability.settings.CameraOptionsDataSource
+import org.pointyware.accountability.settings.CameraOptionsManagerDataSource
+import org.pointyware.accountability.settings.ConfigurationRepository
+import org.pointyware.accountability.settings.LocationAndroidDataSource
+import org.pointyware.accountability.settings.LocationDataSource
+import org.pointyware.accountability.storage.ConfigurableFileProvider
+import org.pointyware.accountability.storage.FileProvider
+
+/**
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+interface RecordingModule {
+
+    @Binds
+    fun getRecordingRepository(androidRecordingRepository: AndroidRecordingRepository): RecordingRepository
+    @Binds
+    fun getConfigurationRepository(androidConfigurationRepository: AndroidConfigurationRepository): ConfigurationRepository
+    @Binds
+    fun getCameraOptionsDataSource(cameraOptionsManagerDataSource: CameraOptionsManagerDataSource): CameraOptionsDataSource
+    @Binds
+    fun getAudioOptionsDataSource(audioDao: AudioDao): AudioOptionsDataSource
+    @Binds
+    fun getLocationDataSource(locationAndroidDataSource: LocationAndroidDataSource): LocationDataSource
+    @Binds
+    fun getCallingOptionsDataSource(callingOptionsAndroidDataSource: CallingOptionsAndroidDataSource): CallingOptionsDataSource
+//    @Binds
+//    fun getSharedPreferencesDataSource(sharedPreferencesAndroidDataSource: SharedPreferencesAndroidDataSource): SharedPreferencesDataSource
+    @Binds
+    fun getRecordingManager(cameraRecordingManager: CameraRecordingManager): RecordingManager
+//    @Binds
+//    fun getNotificationOutput(notificationCoordinator: NotificationCoordinator): NotificationOutput
+//    @Binds
+//    fun getLocalRecordingDao(fileRecordingDao: FileRecordingDao): LocalRecordingDao
+    @Binds
+    fun getFileProvider(configurableFileProvider: ConfigurableFileProvider): FileProvider
+//
+//    @Binds
+//    fun getPreviewSurfaceProvider(androidPreviewSurfaceProvider: AndroidPreviewSurfaceProvider): PreviewSurfaceProvider
+//
+//    @Binds
+//    fun getPermissionVerifier(androidPermissionVerifier: AndroidPermissionVerifier): PermissionVerifier
+//    @Binds
+//    fun getPermissionRequestLauncher(androidPermissionRequestLauncher: AndroidPermissionRequestLauncher): PermissionRequestLauncher
+}
