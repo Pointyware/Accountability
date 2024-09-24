614acaac4dbd7e2e0bdcd8356908324e3287329f
Taush Sampley <taushsampley@gmail.com>
2024-09-24 11:50:41 -0500

    Add basic uuid implementation

--- core/entities/src/commonMain/kotlin/Uuid.kt
+++ core/entities/src/commonMain/kotlin/Uuid.kt
@@ -8,3 +8,18 @@ private const val BYTE_COUNT = 16
 private const val VERSION_INDEX = 6
 private const val VERSION_MASK_INVERSE = 0x0F.toByte()
 private const val VERSION_VALUE_4 = 0x40.toByte()
+
+data class Uuid(
+    val bytes: ByteArray
+) {
+
+    operator fun get(index: Int): Byte = bytes[index]
+
+    companion object {
+        fun v4(): Uuid {
+            val bytes = Random.Default.nextBytes(BYTE_COUNT)
+            bytes[VERSION_INDEX] = (bytes[VERSION_INDEX] and VERSION_MASK_INVERSE) or VERSION_VALUE_4
+            return Uuid(bytes)
+        }
+    }
+}
