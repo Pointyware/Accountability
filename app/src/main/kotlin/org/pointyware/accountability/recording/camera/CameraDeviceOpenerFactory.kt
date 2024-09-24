6f6e6eb9252fd2f752ec26a87db64aa2167846eb
Taush Sampley <taushsampley@gmail.com>
2024-09-24 16:14:02 -0500

    Add camera device openers

---
+++ app/src/main/kotlin/org/pointyware/accountability/recording/camera/CameraDeviceOpenerFactory.kt
@@ -0,0 +1,46 @@
+package org.pointyware.accountability.recording.camera
+
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraManager
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import java.util.concurrent.Executors
+import javax.inject.Inject
+
+/**
+ *
+ */
+class CameraDeviceOpenerFactory @Inject constructor(
+    private val cameraManager: CameraManager
+) {
+    fun createOpener(previewSurface: Surface, recordingSurface: Surface): CameraDeviceOpener {
+
+        val captureCallback = object: CameraCaptureSession.CaptureCallback() {
+            // TODO: implement?
+        }
+
+        return if (Build.VERSION.SDK_INT >= 28) {
+            val executor = Executors.newSingleThreadExecutor()
+            PieCameraDeviceOpener(
+                cameraManager,
+                previewSurface, recordingSurface,
+                PieSessionStateCallback(captureCallback, executor),
+                executor
+            )
+        } else {
+            val looperThread = HandlerThread("${javaClass.simpleName}-HandlerThread")
+            val handler = looperThread.let {
+                it.start()
+                Handler(it.looper)
+            }
+            LegacyCameraDeviceOpener(
+                cameraManager,
+                previewSurface, recordingSurface,
+                LegacySessionStateCallback(captureCallback, handler),
+                handler
+            )
+        }
+    }
+}
