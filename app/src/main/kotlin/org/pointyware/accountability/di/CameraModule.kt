249f363d55111d812fcbf7289418ed70853e1956
Taush Sampley <taushsampley@gmail.com>
2024-09-24 16:27:11 -0500

    Add camera module

---
+++ app/src/main/kotlin/org/pointyware/accountability/di/CameraModule.kt
@@ -0,0 +1,36 @@
+package org.pointyware.accountability.di
+
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.media.MediaRecorder
+import android.os.Handler
+import android.os.HandlerThread
+import androidx.core.content.getSystemService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+/**
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class CameraModule {
+
+    @Provides
+    fun getCameraManager(@ApplicationContext context: Context): CameraManager {
+        return context.getSystemService()!!
+    }
+
+    @Provides
+    fun getMediaRecorder(): MediaRecorder {
+        return MediaRecorder()
+    }
+
+    @Provides
+    fun getHandler(): Handler = HandlerThread("CameraLooper").let { thread ->
+            thread.start()
+            Handler(thread.looper)
+        }
+}
