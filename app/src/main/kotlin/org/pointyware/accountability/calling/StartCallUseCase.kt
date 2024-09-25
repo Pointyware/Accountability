c0c573df4800b0255736d7eaa2ee6f50e6aff568
Taush Sampley <taushsampley@gmail.com>
2024-09-24 19:25:59 -0500

    adapt old call code to use case

--- app/src/main/kotlin/org/pointyware/accountability/calling/StartCallUseCase.kt
+++ app/src/main/kotlin/org/pointyware/accountability/calling/StartCallUseCase.kt
@@ -1,30 +1,32 @@
 package org.pointyware.accountability.calling
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
 
 /**
  * Opens the System dialer with the given number.
  */
 class StartCallUseCase @Inject constructor(
+    @ActivityContext
     private val context: Context
 ) {
     suspend fun invoke(callAfterDial: Boolean, number: String = "") {
-        /*
-
-    @Deprecated("Use StartCallUseCase instead",
-        ReplaceWith("StartCallUseCase.invoke(callAfterDial, number)"))
-    private fun startCall(callAfterDial: Boolean, number: String = "") {
         val action = if (callAfterDial) { Intent.ACTION_CALL } else { Intent.ACTION_DIAL }
         val intent = Intent(action).apply {
             data = Uri.parse("tel:$number")
         }
         try {
-            startActivity(intent)
+            context.startActivity(intent)
         } catch (exception: ActivityNotFoundException) {
-            Toast.makeText(baseContext, "Activity Could Not Be Found to Handle Calling", Toast.LENGTH_LONG).show()
+            Toast.makeText(context,
+                "Activity Could Not Be Found to Handle Calling",
+                Toast.LENGTH_LONG
+            ).show()
         }
-    }
-         */
     }
 }
