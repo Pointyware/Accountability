2a04912f82ad3999181c6cfc6eb4e3b3755beb71
Taush Sampley <taushsampley@gmail.com>
2024-09-24 19:35:17 -0500

    Use application context instead to prevent leaking activity during config changes

--- app/src/main/kotlin/org/pointyware/accountability/calling/StartCallUseCase.kt
+++ app/src/main/kotlin/org/pointyware/accountability/calling/StartCallUseCase.kt
@@ -5,19 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
-import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 /**
  * Opens the System dialer with the given number.
  */
 class StartCallUseCase @Inject constructor(
-    @ActivityContext
+    @ApplicationContext
     private val context: Context
 ) {
     suspend fun invoke(callAfterDial: Boolean, number: String = "") {
         val action = if (callAfterDial) { Intent.ACTION_CALL } else { Intent.ACTION_DIAL }
         val intent = Intent(action).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
             data = Uri.parse("tel:$number")
         }
         try {
