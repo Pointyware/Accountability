eb792a311aa9abbb1170bb9d5cfac7ac6ea6bdee
Taush Sampley <taushsampley@gmail.com>
2024-09-24 19:16:20 -0500

    Add CallingConfig documentation

--- app/src/main/kotlin/org/pointyware/accountability/calling/CallingConfig.kt
+++ app/src/main/kotlin/org/pointyware/accountability/calling/CallingConfig.kt
@@ -1,7 +1,10 @@
 package org.pointyware.accountability.calling
 
 /**
- * Which 
+ * Configuration for calling from the Viewer.
+ *
+ * @property emergencyNumber The number to call in case of emergency.
+ * @property contactNumber The number to call of a friend.
  */
 data class CallingConfig (
     val emergencyNumber: String?,
