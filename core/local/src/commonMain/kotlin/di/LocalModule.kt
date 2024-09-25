dc03268a2bc4ea5d44d03835b679ba90a8041394
Taush Sampley <taushsampley@gmail.com>
2024-09-24 19:51:56 -0500

    remove commented example dependencies

--- core/local/src/commonMain/kotlin/di/LocalModule.kt
+++ core/local/src/commonMain/kotlin/di/LocalModule.kt
@@ -6,14 +6,6 @@ import org.koin.dsl.module
  *
  */
 fun coreLocalModule() = module {
-//    single { DogDao() }
-//    single { DogRepository(get()) }
-//    single { DogInteractor(get()) }
-//    single { DogListInteractor(get()) }
-//    single { DogDetailInteractor(get()) }
-//    single { DogDetailViewModel(get()) }
-//    single { DogListViewModel(get()) }
-//    single { DogDetailViewModel(get()) }
 }
 
 fun dogDaoModule() = module {
