35a9495a8bb7b64fd0ae5ee4d1082e13354d7a3a
Taush Sampley <taushsampley@gmail.com>
2024-09-25 20:44:16 -0500

    remove extraneous calling enabled and emergency number properties

--- app/src/main/kotlin/org/pointyware/accountability/settings/CallingOptionsDataSource.kt
+++ app/src/main/kotlin/org/pointyware/accountability/settings/CallingOptionsDataSource.kt
@@ -9,9 +9,7 @@ import org.pointyware.accountability.calling.CallingConfig
 interface CallingOptionsDataSource {
 
     val callingConfig: CallingConfig
-    fun setEnabled(enabled: Boolean)
     fun setEmergencyEnabled(enabled: Boolean)
-    fun setEmergency(number: String)
     fun setContactEnabled(enabled: Boolean)
     fun setContact(number: String)
 }
