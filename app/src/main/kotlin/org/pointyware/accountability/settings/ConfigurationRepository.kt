fc50bf5571f8151641584732374f8fb406991081
Taush Sampley <taushsampley@gmail.com>
2024-09-25 20:43:54 -0500

    Add docs to ConfigurationRepository

--- app/src/main/kotlin/org/pointyware/accountability/settings/ConfigurationRepository.kt
+++ app/src/main/kotlin/org/pointyware/accountability/settings/ConfigurationRepository.kt
@@ -11,13 +11,18 @@ import org.pointyware.accountability.recording.RecordingConfig
 import org.pointyware.accountability.storage.StorageLocation
 
 /**
- * Maintains configuration information for the following features:
+ * Maintains configuration information for the following feature groups:
  * - Recording configuration
  * - Recording storage location
  * - Calling configuration
+ *
+ * Setters change single configuration options, while getters return the entire configuration.
  */
 interface ConfigurationRepository {
 
+    /**
+     * Returns the current recording configuration with all options.
+     */
     suspend fun getRecordingConfiguration(): RecordingConfig
     suspend fun setAudioEnabled(enabled: Boolean)
     suspend fun setCameraEnabled(enabled: Boolean)
@@ -27,12 +32,16 @@ interface ConfigurationRepository {
     suspend fun getStorageLocation(): StorageLocation
     suspend fun setStorageLocation(location: StorageLocation)
 
+    /**
+     * Returns the current calling configuration with all options.
+     */
     suspend fun getCallingConfiguration(): CallingConfig
-    suspend fun setCallingEnabled(enabled: Boolean)
     suspend fun setEmergencyNumberEnabled(enabled: Boolean)
-    suspend fun setEmergencyNumber(number: String)
     suspend fun setContactNumberEnabled(enabled: Boolean)
     suspend fun setContactNumber(number: String)
 
+    /**
+     * Returns a list of permissions required by the application to support enabled features.
+     */
     suspend fun getRequiredPermissions(): List<Permission>
 }
